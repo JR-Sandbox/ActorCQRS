7e4d51e4d41aa1dca04ef28bf851cc96c70f1e25
Jonathan Robertson <jonathan.robertson@hach.com>
2021-07-26 18:31:55 -0600

    scaffolding

---
+++ ActorCQRS.Actor1.Interfaces/IQueryMeasurement.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ServiceFabric.Actors;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActorCQRS.Actor1.Interfaces
+{
+    public interface IQueryMeasurement : IActor
+    {
+        Task<List<Measurement>> GetMeasurementsAsync();
+    }
+}
---
+++ ActorCQRS.Actor1.Interfaces/Measurement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActorCQRS.Actor1.Interfaces
+{
+    public class Measurement
+    {
+        public double? NumericValue { get; set; }
+        public string StringValue { get; set; }
+        public int Precision { get; set; }
+        public DateTime UTCCapturedTimestamp { get; set; }
+        public DateTime UTCSavedTimestamp { get; set; }
+    }
+}
